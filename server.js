2b409e5e7552f75c99915a0f67f79a95b80adffb
adriancgarcia <adriancgarcia@gmail.com>
2023-06-25 17:03:02 -0500

    completed pass it around

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get("/", (req, res) => {
-    res.send("99 bottles of beer on the wall")
+const port = 3333;
+
+
+app.get("/bottles", (req, res) => {
+    res.send(`99 bottles of beer on the wall.<br>
+    99 bottles of beer.<br>
+    <a href='/bottles/98'>Take one down pass it around.</a>`)
 })
 
-// On the home page (get "/"), users should see:
-// "99 Bottles of beer on the wall"
-// a link that says "take one down, pass it around"
-// this should link to /98, where the number represents the number of bottles left.
-// When a number is given in the url (get "/:number_of_bottles"), users should see:
-// The number of bottles of beer on the wall (i.e. 98 Bottles of beer on the wall.)
-// a link to "take one down, pass it around", where the href is number of bottles in the parameter minus 1.
-// If there are 0 bottles left, do not show a link to "take one down"
-// Add a link to start over, which directs the user back to the home page.
-// Hints
-// You should use an anchor tag with an href to link to the next 'page'
+// I got help on this part by reading other questions in the 'Help' slack channel. //
+app.get("/bottles/:num_of_bottlesLeft", (req, res) => {
+    const bottlesLeft = Number(req.params.num_of_bottlesLeft);
+    if(bottlesLeft > 0) {
+    res.send(`${bottlesLeft} bottles of beer on the wall.<br>
+    <a href="${bottlesLeft - 1}"> Take one down pass it around</a><br>
+    <a href="/bottles">Restart</a>`)
+    }else {
+        res.send(`No more bottles of beer on the wall.<br>
+        <a href="/bottles">Restart</a>`)
+    }
+    // console.log (bottlesLeft)
+})
 
-
-
-app.listen(3333, () => {
-    console.log("listening on port 3333")
+app.listen(port, () => {
+    console.log('Listening on port', port)
 })
